022f61a2a191a100a0c9f8cca40dca9f8e47e609
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-31 20:43:25 +0900

    add color picker

--- src/AddColorForm.js
+++ src/AddColorForm.js
@@ -1,22 +1,26 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import { useInput } from "./hools";
 
 export default function AddColorForm({ onNewColor = (f) => f }) {
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
-    onNewColor(title, color);
-    txtTitle.current.value = "";
-    hexColor.current.value = "";
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        {...titleProps}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input {...colorProps} type="color" required />
       <button>ADD</button>
     </form>
   );
